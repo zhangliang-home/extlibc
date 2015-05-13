7773f848db4940b3f3ad510e15a8886b93fc2a64
zhangliang-home <zhangliang_home@126.com>
2015-05-13 17:05:07 +0800

    add files

--- data_structure/extlibc_list.c
+++ data_structure/extlibc_list.c
@@ -5,4 +5,28 @@
  *  Author: B42965
  */
 
+#include "../include/extlibc_common.h"
+#include "../include/extlibc_list.h"
 
+typedef struct __extlibc_list_node_s
+{
+    void* p_elem;
+    struct __extlibc_list_node_s* p_next;
+} __extlibc_list_node_t;
+
+typedef struct __extlibc_list_s
+{
+    uint32_t elem_cnt;
+    __extlibc_list_node_t* p_list;
+} __extlibc_list_t;
+
+void extlibc_list_init(extlibc_list_ptr_t plist)
+{
+    if(NULL == plist)
+    {
+        return;
+    }
+
+    plist->elem_cnt = 0;
+    plist->p_list = NULL;
+}
